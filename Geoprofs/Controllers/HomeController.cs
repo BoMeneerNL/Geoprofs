87de12b57fcb66902333013065cf04828f1e2107
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-06-16 11:30:01 +0200

    Login page gemaakt

--- Geoprofs/Controllers/HomeController.cs
+++ Geoprofs/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace Geoprofs.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
