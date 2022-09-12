b9356007f54255d48f48102b4a83c83a36a9cca1
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2022-09-12 13:05:28 +0200

    updated to new system

---
+++ Backend/GPAPI/Models/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using GPAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace api.Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Medewerker> Medewerkers { get; set; }
+    }
+}
---
+++ Backend/GPAPI/Models/Medewerker.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace GPAPI.Models
+{
+    public class Medewerker
+    {
+        [Key]
+        public int MedewerkerID { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Naam { get; set; }
+        public string Wachtwoord { get; set; }
+    }
+}
