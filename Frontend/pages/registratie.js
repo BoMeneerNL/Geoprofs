b0f539fe82ccb8439406351deb1371fc6c4ea402
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-09-28 11:09:46 +0200

    werkende registratie toegevoegd

--- Frontend/pages/registratie.js
+++ Frontend/pages/registratie.js
@@ -1,13 +1,14 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
-import Navbar from '../components/navbar';
-import {useRouter} from 'next/router';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Navbar from "../components/navbar";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 const theme = createTheme();
 
@@ -18,29 +19,43 @@ export default function Register() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      name: data.get('name'),
-      password: data.get('password'),
+      name: data.get("name"),
+      password: data.get("password"),
     });
-  }
+    axios
+      .put("http://localhost:11738/Medewerker", {
+        isAdmin: false,
+        naam: data.get("name"),
+        wachtwoord: data.get("password"),
+      })
+      .then(() => {
+        router.push("/");
+      });
+  };
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs">
-          <CssBaseline/>
+          <CssBaseline />
           <Box
             sx={{
               marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
             <Typography component="h1" variant="h5">
               Registreren
             </Typography>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -73,7 +88,7 @@ export default function Register() {
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{mt: 3, mb: 2}}
+                sx={{ mt: 3, mb: 2 }}
               >
                 Registreren
               </Button>
