0442d75f9962ec2dc729b755be88965e1a7f05c6
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-12-12 10:34:46 +0100

    teamid toegevoegd

---
+++ Backend/GPAPI/Migrations/20221212093030_adduserinmigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GPAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduserinmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TeamID",
+                table: "Verlof",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamID",
+                table: "Verlof");
+        }
+    }
+}
