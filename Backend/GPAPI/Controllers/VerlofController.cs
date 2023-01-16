668158cc8ea142bb50f7661c94a51773043ce409
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2023-01-16 11:26:00 +0100

    Verlof controller cleaner gemaakt

--- Backend/GPAPI/Controllers/VerlofController.cs
+++ Backend/GPAPI/Controllers/VerlofController.cs
@@ -15,16 +15,31 @@ namespace GPAPI.Controllers
         [HttpGet]
         public ActionResult GetVerlof()
         {
-            string Naam = "";
-            var res = _context.Verlof.Select(x => new {x.VerlofID, x.TeamID, x.MedewerkerID, x.RedenVerzoek, x.Status, x.Van, x.Tot, Naam }).ToArray();
-            for (int i = 0; i < res.Length; i++)
-            {
-                string tmpnaam = _context.Medewerkers.Where(x => x.MedewerkerID == res[i].MedewerkerID).Select(x => x.Naam).FirstOrDefault();
-                var tempobj = new { res[i].VerlofID, res[i].TeamID, res[i].MedewerkerID, res[i].RedenVerzoek, res[i].Status, res[i].Van, res[i].Tot, Naam = tmpnaam };
-                res[i] = tempobj;
-            }
+            //string Naam = "";
+            //var res = _context.Verlof.Select(x => new {x.VerlofID, x.TeamID, x.MedewerkerID, x.RedenVerzoek, x.Status, x.Van, x.Tot, Naam }).ToArray();
+            //for (int i = 0; i < res.Length; i++)
+            //{
+            //    string tmpnaam = _context.Medewerkers.Where(x => x.MedewerkerID == res[i].MedewerkerID).Select(x => x.Naam).FirstOrDefault();
+            //    var tempobj = new { res[i].VerlofID, res[i].TeamID, res[i].MedewerkerID, res[i].RedenVerzoek, res[i].Status, res[i].Van, res[i].Tot, Naam = tmpnaam };
+            //    res[i] = tempobj;
+            //}
 
-            return Ok(res.ToList());
+            //return Ok(res.ToList());
+
+            var response = _context.Verlof.Include(x => x.Medewerker).Include(x => x.Team).Select(v => new
+            {
+                v.VerlofID,
+                v.MedewerkerID,
+                v.Medewerker.Naam,
+                v.Van,
+                v.Tot,
+                v.Status,
+                v.RedenVerzoek,
+                v.TeamID,
+                teamNaam = v.Team.Naam
+            }).ToList();
+
+            return Ok(response);
         }
         [HttpPut]
         public ActionResult CreateNewVerlof(Verlof verlof)
